b826babd0d8d6503a53786c4064efdd98d7a40ce
Ishani Ved <ishaniv@jfrog.com>
2021-02-11 14:13:38 -0800

    new commit for promote

--- src/main/java/org/test/HelloWorld.java
+++ src/main/java/org/test/HelloWorld.java
@@ -2,6 +2,6 @@ package org.test;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hi3 again from jenkins");
+        System.out.println("New commit for promote test");
     }
 }
