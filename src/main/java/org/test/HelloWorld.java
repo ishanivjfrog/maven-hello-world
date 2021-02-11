5b051f34f2873142e2b9930374a0f80769eb07cb
Ishani Ved <ishaniv@jfrog.com>
2021-02-11 01:57:30 -0800

    added sop Hi again

--- src/main/java/org/test/HelloWorld.java
+++ src/main/java/org/test/HelloWorld.java
@@ -2,6 +2,6 @@ package org.test;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hi from jenkins");
+        System.out.println("Hi again from jenkins");
     }
 }
