7234643ae2090598813be47108c3055af2aa7fcc
Ishani Ved <ishaniv@jfrog.com>
2021-03-24 20:15:14 -0700

    Commit on 24th March for build deploy

--- src/main/java/org/test/HelloWorld.java
+++ src/main/java/org/test/HelloWorld.java
@@ -2,6 +2,6 @@ package org.test;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("New commit for promote test 2");
+        System.out.println("Commit on 24th March for build deploy");
     }
 }
