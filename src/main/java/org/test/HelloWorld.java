90badc26c82eae045240d1e3ceecc1bdef1196ec
Ishani Ved <ishaniv@jfrog.com>
2021-05-24 15:46:28 -0700

    Commit on 24th May for build deploy

--- src/main/java/org/test/HelloWorld.java
+++ src/main/java/org/test/HelloWorld.java
@@ -2,6 +2,6 @@ package org.test;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Commit on 23rd Apr for build deploy");
+        System.out.println("Commit on 24th May for build deploy");
     }
 }
